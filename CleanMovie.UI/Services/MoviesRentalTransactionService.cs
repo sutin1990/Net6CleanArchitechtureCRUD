b98673ac24bb34e02063fcd96e89d0e32b7bbc29
AWARE\sutin.p <sutin.p@aware.co.th>
2023-01-30 16:08:40 +0700

    commit adjust button delete + add notfication

--- CleanMovie.UI/Services/MoviesRentalTransactionService.cs
+++ CleanMovie.UI/Services/MoviesRentalTransactionService.cs
@@ -1,4 +1,5 @@
-﻿using CleanMovie.Domain.PartialModels;
+﻿using CleanMovie.Domain.DBModels;
+using CleanMovie.Domain.PartialModels;
 using CleanMovie.UI.Pages.Component;
 using Microsoft.AspNetCore.Components;
 using Radzen;
@@ -10,18 +11,29 @@ namespace CleanMovie.UI.Services
         private readonly HttpClient _http;
         private readonly NavigationManager _navigationManager;
         private readonly DialogService _dialogService;
+        private readonly NotificationService _notificationService;
 
         public List<DataGridTransactions> MoviesRentalTransaction { get; set; } = new List<DataGridTransactions>();
-        public MoviesRentalTransactionService(HttpClient http, NavigationManager navigationManager, DialogService dialogService)
+        public MoviesRentalTransactionService(HttpClient http, NavigationManager navigationManager, DialogService dialogService, NotificationService notificationService)
         {
             _http = http;
             _navigationManager = navigationManager;
             _dialogService = dialogService;
+            _notificationService = notificationService;
         }
         public async Task GetMoviesRentalTransactionByCriteria(CriteriaTransaction request)
         {
-            var result = await _http.PostAsJsonAsync("api/MovieRentalTransaction/GetMoviesRentalTransactionByCriteria", request);
-            _ = this.SetMoviesRentalTransactionList(result);
+            try
+            {
+                var result = await _http.PostAsJsonAsync("api/MovieRentalTransaction/GetMoviesRentalTransactionByCriteria", request);
+                _ = this.SetMoviesRentalTransactionList(result);
+            }
+            catch(Exception ex)
+            {
+                ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Get Data By Criteria Fail", Detail = $" Get Data By Criteria Fail, Error [{ex.Message}]"});
+                throw;
+            }
+            
         }
 
         public async Task<MoviesRentalTransaction> GetMoviesRentalTransactionById(int id)
@@ -53,37 +65,60 @@ namespace CleanMovie.UI.Services
                 {
                     CriteriaTransaction req = new();
                     await this.GetMoviesRentalTransactionByCriteria(req);
+                    ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Success, Summary = "Create Success", Detail = $"Create Transaction {moviesRentalTransaction?.Movie?.Name} Success", Duration = 4000 });
                 }
                 //_navigationManager.NavigateTo("moviesrentaltrans");
                 //await SetMovie(result);
             }
             catch (Exception ex)
             {
-                throw ex;
+                ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Create Fail", Detail = $"Create Transaction {moviesRentalTransaction?.Movie?.Name} Fail,Error [{ex.Message}]"});
+                throw;
             }
            
         }
 
         public async Task UpdateTransaction(MoviesRentalTransaction moviesRentalTransaction)
-        {            
-            var result = await _http.PutAsJsonAsync("api/MovieRentalTransaction/Update", moviesRentalTransaction);
-            if (result != null)
+        {
+            try
             {
-                CriteriaTransaction req = new();
-                await this.GetMoviesRentalTransactionByCriteria(req);
+                var result = await _http.PutAsJsonAsync("api/MovieRentalTransaction/Update", moviesRentalTransaction);
+                if (result != null)
+                {
+                    CriteriaTransaction req = new();
+                    await this.GetMoviesRentalTransactionByCriteria(req);
+                    ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Success, Summary = "Update Success", Detail = $"Update Transaction {moviesRentalTransaction?.Movie?.Name} Success", Duration = 4000 });
+                }
             }
-            _navigationManager.NavigateTo("moviesrentaltrans");
+            catch (Exception ex)
+            {
+                ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Update Fail", Detail = $"Update Transaction {moviesRentalTransaction?.Movie?.Name} Fail,Error [{ex.Message}]" });
+                throw;
+            }           
+            //_navigationManager.NavigateTo("moviesrentaltrans");
             //await SetMovie(result);
         }
 
         public async Task DeleteTransaction(int id)
         {
-            var result = await _http.DeleteAsync($"api/MovieRentalTransaction/Delete/{id}");
-            if(result != null)
+            try
             {
-                MoviesRentalTransaction.RemoveAll(s=>s.Id == id);
+                var result = await _http.DeleteAsync($"api/MovieRentalTransaction/Delete/{id}");
+                if (result != null)
+                {
+                    MoviesRentalTransaction.RemoveAll(s => s.Id == id);
+                    CriteriaTransaction req = new();
+                    await this.GetMoviesRentalTransactionByCriteria(req);
+                    ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Success, Summary = "Delete Success", Detail = $"Delete Transaction {id} Success", Duration = 4000 });
+                }
             }
-            _navigationManager.NavigateTo("moviesrentaltrans");            
+            catch (Exception ex)
+            {
+                ShowNotification(new NotificationMessage { Severity = NotificationSeverity.Error, Summary = "Delete Fail", Detail = $"Delete Transaction {id} Fail,Error [{ex.Message}]" });
+                throw;
+            }
+            
+            //_navigationManager.NavigateTo("moviesrentaltrans");            
         }
 
         public async Task<MoviesRentalTransaction> OpenEditDialog(DataGridTransactions data)
@@ -95,20 +130,13 @@ namespace CleanMovie.UI.Services
             if (result != null)
             {
                 moviesRentalTransaction = result;
-                //moviesRentalTransaction = new MoviesRentalTransaction
-                //{
-                //    Id = result.Id,
-                //    RentDate = result.RentDate,
-                //    ReturnDate= result.ReturnDate,
-                //    ExpiryDate= result.ExpiryDate,  
-                //    LateFines = result.LateFines,
-                //    MovieId = result.MovieId,
-                //    CutomerId= result.CutomerId,
-                //    StaffRentId= result.StaffRentId,
-                //    StaffReturnId= result.StaffReturnId
-                //};
             }
             return moviesRentalTransaction;
+        }
+
+        private void ShowNotification(NotificationMessage message)
+        {
+            _notificationService.Notify(message);
         }
     }
 }
