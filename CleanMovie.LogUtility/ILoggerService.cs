17f1a4cb8a106feb15deccb67e9f17e3c23e56c0
AWARE\sutin.p <sutin.p@aware.co.th>
2023-02-13 11:44:21 +0700

    commit fixissue add project utility

---
+++ CleanMovie.LogUtility/ILoggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtility
+{
+    public interface ILoggerService
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Error(string message, Exception? ex = null);
+    }
+}
